bd22d7bbc42e5b5585246f5ea3c83da7266e65d7
Arpit Goyal <arpit.go4@gmail.com>
2016-08-16 18:26:41 +0530

    default companies and countries added.

--- app_server/models/mysqlTables.js
+++ app_server/models/mysqlTables.js
@@ -21,7 +21,18 @@ var mysqlTables = {
     country_table :  "DROP TABLE IF EXISTS country;" +
                         "CREATE TABLE IF NOT EXISTS country (" + 
                         "id INT(1) NOT NULL PRIMARY KEY, " +
-                        "countryName VARCHAR(25)" + ")"
+                        "countryName VARCHAR(25)" + ")",
+
+    country_table_data: "INSERT INTO country VALUES (1, 'India');" +
+                        "INSERT INTO country VALUES (2, 'USA');" +
+                        "INSERT INTO country VALUES (3, 'Japan');" +
+                        "INSERT INTO country VALUES (4, 'France');",
+
+    company_table_data: "INSERT INTO company VALUES (1, 'Facebook');" +
+                        "INSERT INTO company VALUES (2, 'Google');" +
+                        "INSERT INTO company VALUES (3, 'Twitter');" +
+                        "INSERT INTO company VALUES (4, 'Xiaomi');"
+
 };
 
 module.exports = mysqlTables;
